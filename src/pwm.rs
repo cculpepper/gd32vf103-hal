17e8b3988a7a118fc6e53ab0943c0b1b750b2929
Chris Culpepper <cculpepper1214@gmail.com>
2020-03-02 06:14:59 -0500

    Compiles, but I didn,t do the right thing.
    The PWM library is not that generic. Previously, I was trying to follow the pwm library in the STM32 hal crate, but that did not work. I believe this PAC is missing some things that that code expects, and I was not able to compile it as my rust is not that good.

--- src/pwm.rs
+++ src/pwm.rs
@@ -1,91 +1,45 @@
 use crate::pac::TIMER4;
-use crate::pac::AFIO;
-use crate::rcu::{Clocks, APB1};
-use crate::timer::Timer;
+use crate::rcu::{Clocks};
 
-use crate::gpio::{self, gpioa, Alternate, Mode, PushPull};
-use crate::gpio::gpioa::*;
 use crate::time::Hertz;
 // use crate::bb;
 use embedded_hal;
 
-use core::mem;
 
-
-// pub struct C1;
-
-pub trait Pins<P>{
-    const C1: bool = false;
-    type Channels;
-}
-
-mod private {
-    pub trait Sealed{}
-}
-pub trait C1<TIMER>: private::Sealed{}
-
-impl private::Sealed for PA0<Alternate<PushPull>>{}
-impl C1<TIMER4> for PA0<Alternate<PushPull>>{}
-impl<TIMER, P1> Pins<C1> for C1 
-    // where P1 : gpio::Mode<Alternate<PushPull>>,
-          // TIMER : TIMER4,
-    where P1: C1<TIMER4>,
-    
-
-{
-
-    const C1:bool = true;
-    type Channels = Pwm<TIMER, C1>;
-}
-
-
-pub struct Pwm<TIMER, CHANNEL> {
-    _channel: CHANNEL,
-    _timer: TIMER,
-}
-
-
-
-impl Timer<TIMER4> {
-    pub fn pwm<P, PINS>(
-        self,
-        _pins: PINS,
-        freq: Hertz,
-        clock: Clocks,
-        ) -> PINS::Channels
-        where
-            PINS: Pins<P>
-        {
-            todo!();
-            // // mapr.modify_mapr(|_, w| w.timer4_remap().bit(REMAP::REMAP == 1));
-
-            // let Self { timer, clock_scaler, clock_frequency } = self;
-            // // Frequency going into the timer is CK_TIMER4
-// // This should be timer4, pins, pwm preq and clock frequency
-            let Self {timer} = self;
-            timer4(timer, _pins, freq.into(), clock.ck_apb1())
-        }
-}
-
-fn timer4<P, PINS>(
+fn init_pwm_4(
+    freq: Hertz,
+    clock: Clocks,
     timer: TIMER4,
-    _pins: PINS,
+    ch1: bool,
+    ch2: bool,
+    ch3: bool,
+    ch4: bool
+    ){
+    init_timer4(&timer, freq.into(), clock.ck_apb1());
+    if ch1{
+        // We're assuming the GPIOs are already in alternate mode here.
+        set_duty(&timer, 1, 0);
+        enable(&timer, 1);
+    }
+    if ch2{
+        set_duty(&timer, 2, 0);
+        enable(&timer, 2);
+    }
+    if ch3{
+        set_duty(&timer, 3, 0);
+        enable(&timer, 3);
+    }
+    if ch4{
+        set_duty(&timer, 4, 0);
+        enable(&timer, 4);
+    }
+}
+
+fn init_timer4(
+    timer: &TIMER4,
     freq: Hertz,
     clk: Hertz,
-    ) -> PINS::Channels where
-        PINS: Pins<P>
-{
-    if PINS::C1 {
-        unsafe {
-            timer.chctl0_output().modify(|_,w|{
-                w
-                    // This is just going to use PWM mode 1, bits 111 in the CH0COMCTL
-                    // It should be good enough for now to get pwm working
-                    .ch0comctl().bits(0b111)
-            });
-        }
-    }
-
+    ){
     let ticks = clk.0 / freq.0;
     let psc = (ticks / (1 << 16)) as u16;
     let car = (ticks / (psc + 1) as u32) as u16;
@@ -100,43 +54,107 @@ fn timer4<P, PINS>(
                          .spm().clear_bit() // Don't stop the counter
                          .cen().set_bit() // Enable the counter
                         );
-        mem::MaybeUninit::uninit().assume_init()
     }
+    // Timer should now be ticking.
 
 }
 
-// Channel 1
-impl embedded_hal::PwmPin for Pwm<TIMER4, C1> {
-    type Duty = u16;
-
-    fn disable(&mut self) {
-        self.timer.chctl2.modify(|_,w|
+pub fn setup(timer: &TIMER4, ch: u8){
+    unsafe {
+        timer.chctl0_output().modify(|_,w|{
             w
-                .ch0en().clear_bit()
-        );
-        // bb::clear(&(*$TIMERX::ptr()).ccer, 0)
+                // This is just going to use PWM mode 1, bits 111 in the CH0COMCTL
+                // It should be good enough for now to get pwm working
+                .ch0comctl().bits(0b111)
+        });
     }
+}
+pub fn disable(timer: &TIMER4, ch: u8) {
+    match ch{
+        1 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch0en().clear_bit()
+                               ),
+        2 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch1en().clear_bit()
+                               ),
+        3 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch2en().clear_bit()
+                               ),
+        4 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch3en().clear_bit()
+                               ),
+        _ => (),
+    }
+}
 
-    fn enable(&mut self) {
-        self.timer.chctl2.modify(|_,w|
-            w
-                .ch0en().set_bit()
-        );
-        // unsafe { bb::set(&(*$TIMERX::ptr()).ccer, 0) }
+pub fn enable(timer: &TIMER4, ch: u8) {
+    match ch{
+        1 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch0en().set_bit()
+                               ),
+        2 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch0en().set_bit()
+                               ),
+        3 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch0en().set_bit()
+                               ),
+        4 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch0en().set_bit()
+                               ),
+        _ => (),
     }
+}
 
-    fn get_duty(&self) -> u16 {
-        // unsafe { (*$TIMERX::ptr()).ccr1.read().ccr().bits() }
-        10
+pub fn get_duty(timer: &TIMER4, ch: u8) -> u16 {
+    // unsafe { (*$TIMERX::ptr()).ccr1.read().ccr().bits() }
+    match ch{
+        1 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch0en().clear_bit()
+                               ),
+        2 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch1en().clear_bit()
+                               ),
+        3 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch2en().clear_bit()
+                               ),
+        4 =>
+            timer.chctl2.modify(|_,w|
+                                w
+                                .ch3en().clear_bit()
+                               ),
+        _ => ()
     }
+    10
+}
 
-    fn get_max_duty(&self) -> u16 {
-        // unsafe { (*$TIMERX::ptr()).arr.read().arr().bits() }
-        10
-    }
+pub fn get_max_duty(timer: &TIMER4, ch: u8) -> u16 {
+    // unsafe { (*$TIMERX::ptr()).arr.read().arr().bits() }
+    10
+}
 
-    fn set_duty(&mut self, duty: u16) {
-        // unsafe { (*$TIMERX::ptr()).ccr1.write(|w| w.ccr().bits(duty)) }
-    }
+pub fn set_duty(timer: &TIMER4, ch: u8, duty: u16) {
+    // unsafe { (*$TIMERX::ptr()).ccr1.write(|w| w.ccr().bits(duty)) }
 }
 
